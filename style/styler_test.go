88883627b38b111b92d39fe90278dcb75d0a2605
Dima Portenko <troublesohard@gmail.com>
2024-03-16 21:47:40 +0200

    fix styler tests

--- style/styler_test.go
+++ style/styler_test.go
@@ -1,7 +1,6 @@
 package style
 
 import (
-	"fmt"
 	"regexp"
 	"testing"
 
@@ -9,40 +8,39 @@ import (
 )
 
 func TestStyler(t *testing.T) {
+	color.NoColor = false
+
 	t.Run("Red FAIL style appliend", func(t *testing.T) {
 		line := "--- FAIL: TestPrettyLines (0.00s)"
-		failRegex := regexp.MustCompile(`(?i)FAIL`)
-		stylerColor := color.New(color.FgRed)
-		styler := Styler{Regexp: *failRegex, Color: *stylerColor}
-
-		expected := "--- \x1b[0m\x1b[31mFAIL\x1b[0m\x1b[31m: TestPrettyLines (0.00s)\x1b[0m"
-		// result := fmt.Sprintf("%q", styler.Line(line))
-		result := fmt.Sprintf("%q", styler.Line(line))
-
-		if result != expected {
-			t.Errorf("\nResult: %q\nExpected: %q\nline: %s", result, expected, line)
+		styler := Styler{
+			Regexp: *regexp.MustCompile(`(?i)FAIL`),
+			Color:  *color.New(color.FgRed),
 		}
+
+		expected := "--- \x1b[31mFAIL\x1b[0m: TestPrettyLines (0.00s)"
+		result := styler.Line(line)
+
+    assertCorrectMessage(t, expected, result)
 	})
 
 	t.Run("Green PASS style appliend", func(t *testing.T) {
 		line := "--- PASS: TestPrettyLines (0.00s)"
-		passRegex := regexp.MustCompile(`(?i)PASS`)
-		var result string
-		if passRegex.MatchString(line) {
-			result = passRegex.ReplaceAllStringFunc(line, func(s string) string {
-        green := color.New(color.FgGreen)
-				t.Errorf("\nResult: %q\nExpected: %q", s, green.Sprintf(s))
-				return green.Sprintf(s)
-			})
+		styler := Styler{
+			Regexp: *regexp.MustCompile(`(?i)PASS`),
+			Color:  *color.New(color.FgGreen),
 		}
 
-		expected := "--- PASS: TestPrettyLines (0.00s) 1"
-		if result != expected {
-			t.Errorf("\nResult: %q\nExpected: %q", line, expected)
-		}
-	})
+		expected := "--- \x1b[32mPASS\x1b[0m: TestPrettyLines (0.00s)"
+		result := styler.Line(line)
 
-	t.Run("Test pretty lines", func(t *testing.T) {
-		t.Errorf("Test is not implemented")
+    assertCorrectMessage(t, expected, result)
 	})
 }
+
+func assertCorrectMessage(t testing.TB, result, expected string) {
+  t.Helper()
+  if result != expected {
+    t.Errorf("\nResult Q: %q, \nResult S: %s\nExpected Q: %q\nExpected S: %s", result, result, expected, expected)
+  }
+}
+
